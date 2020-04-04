7e82c764f698d2ead20f75d603b8b7b963a5235b
Petr Horacek <phoracek@redhat.com>
2020-04-04 18:28:44 +0200

    add ownership and reprovision forms
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- server.py
+++ server.py
@@ -2,8 +2,12 @@ import os
 import subprocess
 
 import web
+from web import form
 
-urls = ("/", "Index")
+urls = (
+    "/", "Index",
+    "/cluster/(.*)/config", "Config",
+)
 app = web.application(urls, globals())
 render = web.template.render("templates/")
 
@@ -18,6 +22,32 @@ class Index:
 
         return render.index(host_name=host_name, clusters=clusters)
 
+    def POST(self):
+        owner_form = build_owner_form(full=True)
+        if owner_form.validates():
+            if owner_form.get("Take").get_value() is not None:
+                new_user = owner_form.get("User").get_value()
+                cluster = owner_form.get("Cluster").get_value()
+                write_cluster_file(cluster, "OWNER", new_user)
+            elif owner_form.get("Release").get_value() is not None:
+                cluster = owner_form.get("Cluster").get_value()
+                write_cluster_file(cluster, "OWNER", "")
+
+        reprovision_form = build_reprovision_form()
+        if reprovision_form.validates():
+            if reprovision_form.get("Reprovision").get_value() is not None:
+                print("REPROVISIONING")
+
+        web.seeother("/")
+
+
+class Config:
+    def GET(self, cluster_name):
+        try:
+            return read_cluster_file(cluster_name, "config")
+        except Exception as exc:
+            raise web.internalerror(exc)
+
 
 def get_host_name():
     try:
@@ -29,20 +59,63 @@ def get_host_name():
 
 def collect_clusters():
     clusters = []
+
     for cluster_dir in os.listdir("clusters"):
-        clusters.append(
-            {
-                "name": read_cluster_file(cluster_dir, "NAME"),
-                "status": read_cluster_file(cluster_dir, "STATUS"),
-                "owner": read_cluster_file(cluster_dir, "OWNER"),
-            }
-        )
+        cluster = {}
+        cluster["name"] = read_cluster_file(cluster_dir, "NAME")
+        cluster["status"] = read_cluster_file(cluster_dir, "STATUS")
+        cluster["owner_form"] = build_owner_form(
+            read_cluster_file(cluster_dir, "OWNER"), cluster["name"])
+        cluster["reprovision_form"] = build_reprovision_form(
+            read_cluster_file(cluster_dir, "OWNER"), cluster["name"])
+        clusters.append(cluster)
+
     return clusters
+
+
+def build_owner_form(user=None, cluster=None, full=False):
+    if full:
+        return form.Form(
+            form.Hidden("Cluster", description="Cluster"),
+            form.Textbox("User", description="User"),
+            form.Button("Release", description="Release", type="submit"),
+            form.Button("Take", description="Take", type="submit"),
+        )
+    elif user:
+        return form.Form(
+            form.Hidden("Cluster", description="Cluster", value=cluster),
+            form.Textbox("User", description="User", value=user, disabled=None),
+            form.Button("Release", description="Release", type="submit"),
+        )
+    else:
+        return form.Form(
+            form.Hidden("Cluster", description="Cluster", value=cluster),
+            form.Textbox("User", description="User", value=user),
+            form.Button("Take", description="Take", type="submit"),
+        )
+
+
+def build_reprovision_form(user=None, cluster=None):
+    if user:
+        return form.Form(
+            form.Hidden("Cluster", description="Cluster", value=cluster),
+            form.Button("Reprovision", description="Reprovision", type="submit"),
+        )
+    else:
+        return form.Form(
+            form.Hidden("Cluster", description="Cluster", value=cluster),
+            form.Button("Reprovision", description="Reprovision", type="submit", disabled=None),
+        )
 
 
 def read_cluster_file(cluster, file_name):
     with open(os.path.join("clusters", cluster, file_name)) as cluster_file:
         return cluster_file.read().strip()
+
+
+def write_cluster_file(cluster, file_name, value):
+    with open(os.path.join("clusters", cluster, file_name), "w") as cluster_file:
+        return cluster_file.write(value)
 
 
 if __name__ == "__main__":
