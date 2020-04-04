35e246625b114bce63acc993c7a1855dbf47612d
Petr Horacek <phoracek@redhat.com>
2020-04-04 15:07:11 +0200

    add templating of index
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- server.py
+++ server.py
@@ -1,15 +1,30 @@
+import subprocess
+
 import web
 
 urls = (
-    '/(.*)', 'Hello'
+    '/', 'Hello'
 )
 app = web.application(urls, globals())
+render = web.template.render('templates/')
+
 
 class Hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+    def GET(self):
+        try:
+            host_name = get_host_name()
+        except Exception as exc:
+            raise web.internalerror(exc)
+        return render.index(host_name=host_name)
+
+
+def get_host_name():
+    try:
+        host_name_raw = subprocess.check_output(['hostname'], stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as exc:
+        raise Exception("Failed obtaining hostname", exc.returncode, exc.output)
+    return str(host_name_raw, 'utf-8').strip()
+
 
 if __name__ == '__main__':
     app.run()
